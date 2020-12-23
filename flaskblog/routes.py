d2d4356e74f40075f347d930ee36a636ada92a2c
Joseph Vitko <josephvitko21@gmail.com>
2020-12-22 21:03:27 -0500

    Finished Python Flask Tutorial Part 5

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,37 +1,41 @@
-from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
-app = Flask(__name__)
+from flask import render_template, url_for, flash, redirect
+from flaskblog import app
+from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.models import User, Post
 
-app.config['SECRET_KEY'] = '868335c85eb1aa9aea2aca3ddf946957'
 
 posts = [
     {
-        'author' : 'Joe Vitko',
-        'title' : 'Post 1',
-        'content' : 'First post content',
-        'date_posted' : 'April 20 2018'
+        'author': 'Joe Vitko',
+        'title': 'Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20 2018'
     },
     {
-        'author' : 'Joe Vitko',
-        'title' : 'Post 2',
-        'content' : 'Second post content',
-        'date_posted' : 'April 21 2018'
+        'author': 'Joe Vitko',
+        'title': 'Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21 2018'
     }
 
 ]
+
 
 @app.route('/')
 @app.route('/home')
 def index():
     return render_template('index.html', posts=posts)
 
+
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
 
+
 @app.route('/hello/<name>')
 def hello(name):
     return render_template('page.html', name=name)
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -40,6 +44,7 @@ def register():
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('index'))
     return render_template('register.html', title='Register', form=form)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -51,7 +56,4 @@ def login():
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
 
