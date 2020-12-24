5d66e26d677e7fbc4308c2e6a30af04421030c85
Joseph Vitko <josephvitko21@gmail.com>
2020-12-24 17:42:26 -0500

    Finished Python Flask Tutorial Part 10

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '868335c85eb1aa9aea2aca3ddf946957'
@@ -11,5 +12,11 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'accounts@alualgae.com'
+app.config['MAIL_PASSWORD'] = '3ddf946957'
+mail = Mail(app)
 
 from flaskblog import routes
