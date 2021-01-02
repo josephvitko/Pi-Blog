1f2d901ded1653d67cae4e00cab7227f9ab9c72e
Joseph Vitko <josephvitko21@gmail.com>
2021-01-01 23:15:37 -0500

    Created working (but a bit buggy) website pages for each admin function

--- flaskblog/admin/routes.py
+++ flaskblog/admin/routes.py
@@ -1,30 +1,35 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash, url_for, redirect
 from flaskblog import db
 from flaskblog.models import User, Role, UserRoles, Post
+from flaskblog.admin.forms import RoleForm
 
 admin = Blueprint('admin', __name__)
 
 
 @admin.route('/admin')
 def admin_dashboard():
-    pass
+    return render_template('admin_dashboard.html')
 
 
 @admin.route('/admin/db/createall', methods=['GET', 'POST'])
 def createall():
     db.create_all()
-    return 'created all'
+    flash('Database models created', "success")
+    return redirect(url_for('admin.admin_dashboard'))
 
 
 @admin.route('/admin/db/dropall')
 def dropall():
     db.drop_all()
-    return 'dropped all'
+    flash('Database data dropped', "success")
+    return redirect(url_for('admin.admin_dashboard'))
 
 
 @admin.route('/admin/posts')
 def getposts():
-    return str(Post.query.order_by(Post.date_posted.desc()).limit(25).all())
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(per_page=25)
+    return render_template('admin_posts_dashboard.html', posts=posts, page=page, page_context='admin.getposts')
 
 
 @admin.route('/admin/posts/<int:id>')
@@ -37,40 +42,55 @@ def viewpost(id):
 def removepost(id):
     Post.query.filter_by(id=id).delete()
     db.session.commit()
-    return str('deleted post ' + str(id))
+    flash('Post deleted', 'success')
+    return redirect(url_for('admin.getposts'))
 
 
-@admin.route('/admin/roles')
+@admin.route('/admin/roles', methods=['GET', 'POST'])
 def getroles():
-    roles = Role.query.all()
-    return str(roles)
+    form = RoleForm()
+    if form.validate_on_submit():
+        return redirect(url_for('admin.newrole', name=form.name.data))
+    else:
+        flash('Role is invalid', 'danger')
+    roles = Role.query.paginate(per_page=25)
+    return render_template('admin_roles_dashboard.html', roles=roles, form=form, page_context='admin.getroles')
 
 
-@admin.route('/admin/roles/new/<string:name>')
+@admin.route('/admin/roles/new/<string:name>', methods=['GET', 'POST'])
 def newrole(name):
     nr = Role(name=name)
     db.session.add(nr)
     db.session.commit()
-    return str(name + ' added')
+    flash('Role has been created', 'success')
+    return redirect(url_for('admin.getroles'))
 
 
 @admin.route('/admin/roles/drop/<string:name>')
 def droprole(name):
     Role.query.filter_by(name=name).delete()
     db.session.commit()
-    return str(name + ' dropped')
+    flash('Role has been dropped', 'success')
+    return redirect(url_for('admin.getroles'))
 
 
 @admin.route('/admin/users')
 def getusers():
-    users = User.query.limit(100).all()
-    return str(users)
+    page = request.args.get('page', 1, type=int)
+    users = User.query.order_by(User.id.asc()).paginate(per_page=25)
+    return render_template('admin_users_dashboard.html', users=users, page=page, page_context='admin.getusers')
 
 
-@admin.route('/admin/users/<string:user>')
+@admin.route('/admin/users/<string:user>', methods=['GET', 'POST'])
 def viewuser(user):
     user = User.query.filter_by(username=user).first()
-    return str(user)
+    roles = Role.query.all()
+    form = RoleForm()
+    if form.validate_on_submit():
+        return redirect(url_for('admin.assignrole', role=form.name.data, user=user.username))
+    else:
+        flash('Role is invalid', 'danger')
+    return render_template('admin_user_dashboard.html', user=user, roles=roles, form=form)
 
 
 @admin.route('/admin/users/<string:user>/roles')
@@ -86,13 +106,18 @@ def getuserposts(user):
     return str(posts)
 
 
-@admin.route('/admin/users/<string:user>/roles/assign/<string:role>')
+@admin.route('/admin/users/<string:user>/roles/assign/<string:role>', methods=['GET', 'POST'])
 def assignrole(user, role):
     user = User.query.filter_by(username=user).first()
     role = Role.query.filter_by(name=role).first()
-    user.roles.append(role)
-    db.session.commit()
-    return str(str(user) + ' assigned ' + str(role))
+    if role is None:
+        flash('Role does not exist', 'danger')
+        return redirect(url_for('admin.viewuser', user=user.username))
+    else:
+        user.roles.append(role)
+        db.session.commit()
+        flash('Role assigned', 'success')
+        return redirect(url_for('admin.viewuser', user=user.username))
 
 
 @admin.route('/admin/users/<string:user>/roles/revoke/<string:role>')
@@ -101,4 +126,5 @@ def revokerole(user, role):
     role = Role.query.filter_by(name=role).first()
     user.roles.remove(role)
     db.session.commit()
-    return str(str(role) + ' revoked from ' + str(user))
+    flash('Role has been revoked', 'success')
+    return redirect(url_for('admin.viewuser', user=user.username))
