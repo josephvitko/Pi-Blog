469e342d2494b3d7de9f9c76e14c78bbd3e28c00
Joseph Vitko <josephvitko21@gmail.com>
2021-01-04 03:55:06 -0500

    cleaned up

--- flaskblog/weather_data/routes.py
+++ flaskblog/weather_data/routes.py
@@ -47,4 +47,4 @@ def graph(sensor):
     for datum in sensor_data:
         entry = {'x': datum.date_recorded.strftime('%Y-%m-%d %H:%M:%S'), 'y': round(datum.value, 2)}
         chart_data.append(entry)
-    return render_template('sensor_graph.html', chart_data=chart_data)
+    return render_template('sensor_graph.html', chart_data=chart_data, title=sensor.name.capitalize(), unit=sensor.unit)
